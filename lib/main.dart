8189501acf62c8de91d360e00582d1c8be2be8fd
Mithun Nath <mithun@artifaktdigital.com>
2019-08-18 23:18:48 -0400

    project files

---
+++ lib/main.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+import './queries.dart' as queries;
+
+void main() => runApp(GraphQLApp());
+
+class GraphQLApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+
+    final HttpLink link = HttpLink(
+      uri: 'http://api.lyrichords.com/graphql',
+    );
+
+    final ValueNotifier<GraphQLClient> client = ValueNotifier<GraphQLClient>(
+      GraphQLClient(
+        cache: InMemoryCache(),
+        link: link,
+      ),
+    );
+
+    return GraphQLProvider(
+      client: client,
+      child: CacheProvider(
+        child: MaterialApp(
+          title: 'LYRICHORDS',
+          home: Wrapper(),
+        ),
+      ),
+    );
+
+  }
+}
+
+class Wrapper extends StatefulWidget {
+  @override
+  _WrapperState createState() => _WrapperState();
+}
+
+class _WrapperState extends State<Wrapper> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('LYRICHORDS'),
+      ),
+      body: Container(
+        child: Query(
+          options: QueryOptions(
+            document: queries.readSongName,
+            pollInterval: 4,
+          ),
+          builder: (QueryResult result, { VoidCallback refetch, FetchMore fetchMore }){
+            // added VoidCallback later to fix errors on the same line
+            if(result.loading) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+
+            if(result.hasErrors){
+              return Text('\nErrors:\n '+result.errors.join(',\n '));
+            }
+
+            final List<dynamic> pls = result.data["songs"]["edges"];
+            //final String pls = result.data['User']['name'];
+
+            //print(result.data['User']['name']);
+
+            return ListView.builder(
+              itemCount: pls.length,
+              itemBuilder: (BuildContext context, int index){
+                final Map<String, dynamic> pl = pls[index];
+                final String name = pl["node"]["songDetails"]["songInMalayalam"];
+
+                return ListTile(
+                  title: Text('$name'),
+                  trailing: Icon(Icons.thumb_up),
+                  onTap: (){},
+                );
+
+              },
+            );
+
+          },
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/queries.dart
@@ -0,0 +1,66 @@
+// Sample Query 
+const readSongName = '''
+  query readSongName {
+    songs{
+      edges{
+        node{
+          title
+          songDetails{
+            songInMalayalam
+          }
+        }
+      }
+    }
+  }
+''';
+
+//Get All Song Title & ID (both Malayalam and English)
+
+const getAllSongsTitle = '''
+  query getAllSongsTitle {
+    songs{
+      edges{
+        node{
+          songId
+          songDetails{
+            nameEnglish
+            nameMalayalam
+          }
+        }
+      }
+    }
+  }
+''';
+
+// Get All Song Title & and Image
+
+const getAllSongsTitleAndImage = '''
+  query getAllSongsTitleAndImage {
+    songs {
+      edges {
+        node {
+          songId
+          songDetails {
+            nameEnglish
+            nameMalayalam
+            movie {
+              ... on Movie {
+                id
+                movieDetails {
+                  images {
+                    link
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+''';
+
+// Get complete Song details
+
+const getCompleteSongDetails = '''
+''';
